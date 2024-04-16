368dd21c36ab145d1223794ae31999d1c1ff62b5
legendarygamemaker <138945240+legendarygamemaker@users.noreply.github.com>
2024-04-16 19:38:30 +1000

    Character movement update
    pls add time limit on dash

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+var SPEED = 300.0
 const JUMP_VELOCITY = -500.0
+var Jump_count = 0
+var max_jumps = 2
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,8 +15,15 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if (Input.is_action_just_pressed("move_up") or Input.is_action_just_pressed("ui_accept")) and Jump_count < max_jumps:
+		Jump_count += 1
 		velocity.y = JUMP_VELOCITY
+	elif Jump_count >= max_jumps and is_on_floor():
+		Jump_count = 0
+		
+		
+	if Input.is_action_pressed("dash"):
+		SPEED = 555
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("move_left", "move_right")
@@ -23,6 +33,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
+	SPEED = 300
 	var plasma_blaster_equiped = true
 	var shoot_cooldown = true
 	var Plasma_bullet = preload("res://Scenes/plasma_bullet.tscn")
